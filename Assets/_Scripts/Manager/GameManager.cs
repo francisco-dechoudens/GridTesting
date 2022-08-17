1d426d04877e9f3059bf29bde3d2d4ae1e2503bd
Francisco <francisco.dechoudens@abarcahealth.com>
2022-08-16 23:10:59 -0400

    Complete tutorial

---
+++ Assets/_Scripts/Manager/GameManager.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public GameState State;
+
+    public static event Action<GameState> OnGameStateChanged;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        ChangeState(GameState.GenerateGrid);
+    }
+
+    public void ChangeState(GameState newState)
+    {
+        State = newState;
+
+        switch (newState)
+        {
+            case GameState.GenerateGrid:
+                GridManager.Instance.GenerateGrid();
+                break;
+            case GameState.SpawnHeroes:
+                UnitManager.Instance.SpawnHeroes();
+                break;
+            case GameState.SpawnEnemies:
+                UnitManager.Instance.SpawnEnemies();
+                break;
+            case GameState.HeroesTurn:
+                break;
+            case GameState.EnemiesTurn:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+
+        }
+
+        OnGameStateChanged?.Invoke(newState);
+    }
+}
+
+public enum GameState
+{
+    GenerateGrid,
+    SpawnHeroes,
+    SpawnEnemies,
+    HeroesTurn,
+    EnemiesTurn
+}
